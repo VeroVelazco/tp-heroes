849c3e14ac1e039ae92a9e2adb4eeb250aca6d3f
VeroVelazco <veroverona31@gmail.com>
2022-06-25 20:44:13 -0300

    agregue biograrfias

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ app.use(express.static('public'))
 
 
 /*rutas*/
-app.get('/index.html', (req,res)=> res.sendFile(path.join(__dirname,'views','index.html')));
-app.get('/babbage.html', (req,res)=> res.sendFile(path.join(__dirname,'views','babbage.html')));
-app.get('/berners-lee.html', (req,res)=> res.sendFile(path.join(__dirname,'views','berners-lee.html')));
-app.get('/clarke.html', (req,res)=> res.sendFile(path.join(__dirname,'views','clarke.html')));
-app.get('/hamilton.html', (req,res)=> res.sendFile(path.join(__dirname,'views','hamilton.html')));
-app.get('/hopper.html', (req,res)=> res.sendFile(path.join(__dirname,'views','hopper.html')));
-app.get('/lovelace.html', (req,res)=> res.sendFile(path.join(__dirname,'views','lovelace.html')));
-app.get('/turing.html', (req,res)=> res.sendFile(path.join(__dirname,'views','turing.html')));
+app.get('/', (req,res)=> res.sendFile(path.join(__dirname,'views','index.html')));
+app.get('/babbage', (req,res)=> res.sendFile(path.join(__dirname,'views','babbage.html')));
+app.get('/berners-lee', (req,res)=> res.sendFile(path.join(__dirname,'views','berners-lee.html')));
+app.get('/clarke', (req,res)=> res.sendFile(path.join(__dirname,'views','clarke.html')));
+app.get('/hamilton', (req,res)=> res.sendFile(path.join(__dirname,'views','hamilton.html')));
+app.get('/hopper', (req,res)=> res.sendFile(path.join(__dirname,'views','hopper.html')));
+app.get('/lovelace', (req,res)=> res.sendFile(path.join(__dirname,'views','lovelace.html')));
+app.get('/turing', (req,res)=> res.sendFile(path.join(__dirname,'views','turing.html')));
 
 
 
 /*servidor*/
-app.listen(port, () =>console.log('Server running in http://localhost:'+ port + '/index.html'));
+app.listen(port, () =>console.log('Server running in http://localhost:'+ port));
